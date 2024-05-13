2aa3db05fa9195c8fa4e55e30a529714f69b24a2
maxgmr <mxzmxz120@gmail.com>
2024-05-13 16:56:17 -0700

    Add more colour consts in i3 config, add kitty config

--- backup-config.sh
+++ backup-config.sh
@@ -17,5 +17,12 @@ else
 fi
 cp ~/.config/i3blocks/config "$SCRIPT_DIR/i3blocksconfig" && echo "i3blocks config backed up successfully."
 
+if test -f "$SCRIPT_DIR/kittyconfig"; then
+    echo "Overwriting kitty config..."
+else
+    echo "Backing up kitty config..."
+fi
+cp ~/.config/kitty/kitty.conf "$SCRIPT_DIR/kittyconfig" && echo "kitty config backed up successfully."
+
 echo "Backing up i3blocks scripts..."
 find ~/computer/i3blocks-scripts -mindepth 1 -maxdepth 1 -path ~/computer/i3blocks-scripts/venv -prune -o -print | grep -v ~$ | xargs -I{} cp {} ~/computer/dev/i3wmconfig/i3blocks-scripts/ && echo "i3blocks scripts backed up successfully."
